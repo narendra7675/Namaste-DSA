8f9afcaa478d2e6ea7383bf2f47f1c8c9cb1783c
narendra7675 <narendrapathala@gmail.com>
2026-05-01 23:27:05 +0530

    added loop within loop and one start pattern

---
+++ Warm Up/10_startPattern.js
@@ -0,0 +1,11 @@
+let n = 4;
+for(let i=0; i<n; i++){
+    let row = '';
+    for(let j=0; j<n;j++){
+        row = row + '*';
+    }
+    console.log(row);
+}
+
+//i is responsible for how many rows we have.
+//j is responsible for how many columns we have.
