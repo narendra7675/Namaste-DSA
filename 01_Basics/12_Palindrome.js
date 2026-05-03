ed27595db2818f0d7dcec059b1c3877ec635a532
narendra7675 <narendrapathala@gmail.com>
2026-05-03 23:51:39 +0530

    added palindrome number notes

---
+++ 01_Basics/12_Palindrome.js
@@ -0,0 +1,54 @@
+/**
+ * Palindrome Number:
+ * An integer is a palindrome when it reads the same forward and backward.
+ * for example, 121 is a palindrome while 123 is not.
+ * 121 -> from left to right, its 121.
+ *     -> from right to left, it's 121.
+ * 1443441 -> read it reverse, its a palindrome.
+ * 143, reverse of 143 -> 341. It's not palindrome.
+ * 
+ * reverse of number == number then we can say as palindrome number.
+ */
+
+
+/**
+ * How do you find last digit from number?
+ * number % 10 = last digit
+ * number / 10 = removing last digit
+ */
+
+/**
+ * n % 10 and n / 10, these are two different arithmetic operators used in programming and mathematics.
+ * 
+ * 1. Modulus Operator %
+ * n % 10 gives the remainder after dividing n by 10.
+ * 
+ * Example:
+ * 57 % 10 = 7
+ * 123 % 10 = 3
+ * 
+ * why?
+ * Because
+ * 57 % 10 = 5 remainder 7
+ * 123 % 10 = 12 remainder 3
+ * Common use cases:
+ * 1. Getting the last digit of a number.
+ * 2. Checking even/odd numbers.
+ * Cyclic logic (like wrapping values)
+ * 
+ * 
+ * 
+ * 2. Divison Operator /
+ * n / 10 gives the quotient after dividing n by 10.
+ * Example:
+ * 57 / 10 = 5.7
+ * In integer division (many programming languages): 57 / 10 = 5
+ * 
+ * Common use cases:
+ * 1. Scaling values.
+ * 2. Calculating averages.
+ * 3. Removing digits from a number (integer division)
+ */
+
+
+
